ea8b3fecad19568d68dfe0077e99c515ab625b33
amirbaa1 <amir.baahklag@yahoo.com>
2024-02-11 11:59:45 +0330

    add identity ocelot

--- src/Ocelot.ApiGate/Program.cs
+++ src/Ocelot.ApiGate/Program.cs
@@ -1,10 +1,34 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("Ocelot.json", optional: false, reloadOnChange: true);
+
+//------------ identity ---------------//
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:7015"; //  Identity.API
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+           new SymmetricSecurityKey(
+               Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("TokenAuthAPI:JWTOption:Secret")!)),
+            ValidateLifetime = true,
+            ValidateIssuer = true,
+            ValidIssuer = "coin_api",
+            ValidAudience = "coin_client",
+            ClockSkew = TimeSpan.Zero,
+        };
+    });
+//--------------------------------------------//
+
 
 builder.Services.AddOcelot();
 
